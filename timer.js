3d63e316e7b5500c2d80e7cdd465d3dfd6eb8628
Jack Maddigan <jack.g.maddigan@gmail.com>
2023-12-02 13:15:35 +1300

    fixed audio again

--- timer.js
+++ timer.js
@@ -2,14 +2,15 @@ var timerOn = false;
 var stopwatchInterval;
 var startTime;
 var seconds;
-const eightSec = new Audio("audio/eight.mp3");
-const twelveSec = new Audio("audio/twelve.mp3");
+var isPlaying = false;
+const inspectionAudio = new Audio("audio/inspection-audio.mp3");
 
 function updateStopwatch() {
   seconds = Math.floor((Date.now() - startTime) / 1000);
   document.getElementById("timerText").innerHTML = seconds;
   if (seconds >= 15) {
     timerOn = false;
+    isPlaying = false;
     clearInterval(stopwatchInterval);
     document.body.style.backgroundColor = "#470b04";
   } else if (seconds >= 12) {
@@ -19,43 +20,29 @@ function updateStopwatch() {
   }
 }
 
-function eight() {
-  // Play the audio
-  eightSec.volume = 1;
-  eightSec.play();
-  clearInterval(eightSecondsInt);
-}
-
-function twelve() {
-  // Play the audio
-  twelveSec.volume = 1;
-  twelveSec.play();
-  clearInterval(twelveSecondsInt);
-}
-
 function manageTimer() {
   document.body.style.backgroundColor = "green";
   if (timerOn) {
     // stop timer
     clearInterval(stopwatchInterval);
-    clearInterval(eightSecondsInt);
-    clearInterval(twelveSecondsInt);
     timerOn = false;
   } else {
     // start timer
     stopwatchInterval = setInterval(updateStopwatch, 100);
-    eightSecondsInt = setInterval(eight, 8000);
-    twelveSecondsInt = setInterval(twelve, 12000);
     startTime = Date.now();
     timerOn = true;
   }
 }
 
 document.addEventListener("keydown", function (event) {
-  // eightSec.volume = 0;
-  // eightSec.play();
-  // twelveSec.volume = 0;
-  // twelveSec.play();
+  if (!isPlaying) {
+    inspectionAudio.currentTime = 0;
+    inspectionAudio.play();
+    isPlaying = true;
+  } else {
+    inspectionAudio.pause();
+    isPlaying = false;
+  }
   if (event.key === " ") {
     // The space key was pressed
     manageTimer();
@@ -63,9 +50,14 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.addEventListener("touchstart", function (event) {
-  // eightSec.volume = 0;
-  // eightSec.play();
-  // twelveSec.volume = 0;
-  // twelveSec.play();
+  if (!isPlaying) {
+    inspectionAudio.currentTime = 0;
+    inspectionAudio.play();
+    isPlaying = true;
+  } else {
+    inspectionAudio.pause();
+    isPlaying = false;
+  }
+
   manageTimer();
 });
